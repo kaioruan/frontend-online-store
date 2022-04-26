a1d142dcf957ed7c7af3ed2140c8827c1f20a61c
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-04-26 19:02:44 -0300

    [Grupo 13] Requisito 5

--- src/App.js
+++ src/App.js
@@ -2,19 +2,50 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
-import { getCategories } from './services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       categorias: [],
+      btnIsLocked: true,
+      search: '',
+      produtos: [],
     };
     this.fethcCategorias = this.fethcCategorias.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.btnHandler = this.btnHandler.bind(this);
+    this.clickSearch = this.clickSearch.bind(this);
   }
 
   componentDidMount() {
     this.fethcCategorias();
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => this.btnHandler());
+  }
+
+  btnHandler() {
+    const { search } = this.state;
+    if (search.length > 0) {
+      this.setState({ btnIsLocked: false });
+    } else {
+      this.setState({ btnIsLocked: true });
+    }
+  }
+
+  async clickSearch() {
+    const { search } = this.state;
+    const result = await getProductsFromCategoryAndQuery(undefined, search);
+    this.setState({
+      produtos: result.results,
+    });
   }
 
   async fethcCategorias() {
@@ -23,12 +54,23 @@ class App extends React.Component {
   }
 
   render() {
-    const { categorias } = this.state;
+    const { categorias, btnIsLocked, search, produtos } = this.state;
     return (
       <main>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" render={ () => <Home categ={ categorias } /> } />
+            <Route
+              exact
+              path="/"
+              render={ () => (<Home
+                categ={ categorias }
+                btnIsLocked={ btnIsLocked }
+                search={ search }
+                produtos={ produtos }
+                onChange={ this.handleChange }
+                onClick={ this.clickSearch }
+              />) }
+            />
             <Route exact path="/cart" render={ () => <Cart /> } />
           </Switch>
         </BrowserRouter>
