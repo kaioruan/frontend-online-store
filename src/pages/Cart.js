1e6050856fda10205557a3d2708a17b23f4f4dba
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-04-27 20:10:46 -0300

    [Grupo 13] Adianta requisito 10

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,17 +4,36 @@ import PropTypes from 'prop-types';
 class Cart extends React.Component {
   render() {
     const { cartList } = this.props;
+
+    const resultado = cartList.reduce((acc, crr) => {
+      if (acc[`produto${crr.id}`] === undefined) {
+        acc[`produto${crr.id}`] = {
+          descricao: crr,
+          quantidade: 1,
+        };
+      } else {
+        acc[`produto${crr.id}`] = {
+          descricao: crr,
+          quantidade: acc[`produto${crr.id}`].quantidade + 1,
+        };
+      }
+
+      return acc;
+    }, {});
+
+    const cart = Object.values(resultado);
+
     return (
       <div>
         <div>
-          {cartList.length > 0
-            ? cartList.map((value) => (
-              <div key={ value.id }>
-                <p data-testid="shopping-cart-product-name">{ value.title }</p>
-                <img src={ value.thumbnail } alt={ value.title } />
-                <p>{ `R$: ${value.price}` }</p>
+          {cart.length > 0
+            ? cart.map((value) => (
+              <div key={ value.descricao.id }>
+                <p data-testid="shopping-cart-product-name">{ value.descricao.title }</p>
+                <img src={ value.descricao.thumbnail } alt={ value.descricao.title } />
+                <p>{ `R$: ${value.descricao.price}` }</p>
                 <p data-testid="shopping-cart-product-quantity">
-                  {(cartList.filter((el) => el.id === value.id)).length}
+                  {value.quantidade}
                 </p>
               </div>))
             : <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>}
